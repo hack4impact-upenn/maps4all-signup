578e6928b9170239bd22f11303880aba440ca9d2
Ben Sandler <ben1sandler@gmail.com>
2017-11-18 15:00:44 -0500

    beginning of cloudflare stuff

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,6 @@
 from flask import url_for
 
+import CloudFlare
 
 def register_template_utils(app):
     """Register Jinja 2 helpers (called from __init__.py)."""
@@ -18,3 +19,20 @@ def register_template_utils(app):
 
 def index_for_role(role):
     return url_for(role.index)
+
+
+# TODO: move this to a better home
+def register_subdomain(subdomain, target):
+    """
+    :subdomain is the the subdomain name
+    :target is the aliased url
+    """
+    cf = CloudFlare.CloudFlare(Config.CF_API_EMAIL, Config.CF_API_KEY)
+
+    dns_record = {
+        'type': 'CNAME',
+        'name': subdomain,
+        'content': target,
+    }
+
+    cf.zones.dns_records.post(Config.CF_ZONE_ID, data=dns_record)
