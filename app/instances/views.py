4e5cd586a4bf0963609d8bd76a06c041fa63aa3e
Hana Pearlman <hanap@sas.upenn.edu>
2018-02-16 22:30:18 -0500

    pep8

--- app/instances/views.py
+++ app/instances/views.py
@@ -112,8 +112,8 @@ def launch():
 
 
 @csrf.exempt
-@instances.route('/send-admin-email/<email>/<password>/<name>', 
-    methods=['GET', 'POST'])
+@instances.route(
+    '/send-admin-email/<email>/<password>/<name>', methods=['GET', 'POST'])
 def send_admin_email(email, password, name):
     get_queue().enqueue(
         send_email,
