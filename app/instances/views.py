235328c8c36d2f94f066b0b75ab1e30f78ebcfe2
Hana Pearlman <hanap@sas.upenn.edu>
2018-06-08 22:53:06 -0700

    send new admin a link to reset their password when they create a new instance with fixed style

--- app/instances/views.py
+++ app/instances/views.py
@@ -131,7 +131,7 @@ def get_status(app_setup_id, auth):
 
 @csrf.exempt
 @instances.route('/send-admin-email/<email>/<password>/<name>',
-                methods=['POST'])
+                 methods=['POST'])
 def send_admin_email(email, password, name):
     get_queue().enqueue(
         send_email,
